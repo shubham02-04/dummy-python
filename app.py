5cadc171b8d5f3d713135a66afe97f8cd55aa2e1
shubham02-04 <shubham.mgujarati@gmail.com>
2026-06-24 13:18:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,5 @@ def about():
 
 # Run the local development server
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
